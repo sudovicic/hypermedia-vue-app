bcec28c40c56a38a4f1e9ec63761640dc4d178a5
Ernes Ibrovic <ernes.ibrovic@506.ai>
2023-01-07 16:57:42 +0100

    design updates and translations

--- src/pages/Create.vue
+++ src/pages/Create.vue
@@ -9,8 +9,8 @@ const { t } = useI18n();
 <template>
   <NavBar headline-key="create_page_name" />
   <main>
-    <div class="flex mt-8 mx-4 w-screen">
-      <div class="flex flex-col justify-center gap-y-4">
+    <div class="flex mt-8 mx-4 w-screen justify-center">
+      <div class="flex flex-col gap-y-4">
         <h1 class="font-bold">{{ t("create_headline") }}</h1>
         <CreateForm />
       </div>
